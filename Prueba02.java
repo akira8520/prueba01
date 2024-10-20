215fab852d3af1973daaaaa7ef67a92a8dcb6cf7
= <u23271982@utp.edu.pe>
2024-10-20 13:29:21 -0500

    Tercera prueba de lo mismo

--- Prueba02.java
+++ Prueba02.java
@@ -1,5 +1,6 @@
 public class Prueba02 {
     public static void main(String[] args) {
         System.out.println("Prueba para ver si las ramas derivadas se pueden guardar en GitHub");
+        System.out.println("tercera prueba de lo mismo");
     }
 }
